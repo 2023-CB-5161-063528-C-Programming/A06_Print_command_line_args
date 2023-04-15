f88ddccc525b9ef5b29cc3d9d69de306c6685acc
Dmitry V. Luciv <dluciv@dluciv.name>
2023-04-15 10:30:03 +0300

    print_args.c

---
+++ print_args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>  // for strlen & strcat
+
+int main(int argc, const char *argv[])
+{
+  // ....
+  int tot = 2086678; // ...really calculate total length or args + spaces between using strlen fnuction
+  // ....
+
+  char a[tot + 1];
+  a[0] = 0;
+
+  // "./a.out" "1" "2" "3" "456" -- strcat --> "./a.out 1 2 3 456"
+  puts(a);
+
+  return 0;
+}
